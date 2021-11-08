6accd120b44c6b6509ac22b8c1d1a4c139a69d4f
Kaisar <kaisarb@gmail.com>
2021-11-08 15:37:37 +0600

    work with packages. create class object
    work with packages. create class object

---
+++ B.java
@@ -0,0 +1,8 @@
+package com.kb.packages;
+import com.kb.packages.*;
+class B{
+	public static void main(String args[]) {
+		HiPackages myobj = new HiPackages();
+		myobj.msg();
+	}
+}
---
+++ HiPackages.java
@@ -0,0 +1,7 @@
+package com.kb.packages;
+class HiPackages{
+	//public static void main(String args[]) {
+	public void msg() {	
+		System.out.println("HiPackages");
+	}
+}
