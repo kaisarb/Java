7e803705679e2c6e5e4530f13de2ff7a56b31bde
Kaisar <kaisarb@gmail.com>
2021-11-20 10:38:02 +0600

    Reading and Writing Object Fields
    It’s possible to read and write instance variables directly from the caller. In this example, a mother swan lays eggs

---
+++ Swan.java
@@ -0,0 +1,10 @@
+package com.kb.packages;
+public class Swan {
+	int numberEggs;
+	public static void main(String[] args) {
+		Swan mother = new Swan();
+		mother.numberEggs =  1;  //set variable
+		System.out.println(mother.numberEggs);  // read variable
+		
+	}
+}
