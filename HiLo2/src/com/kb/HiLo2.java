5227a2f370cefd8205a2abe99d738ad059ccee8f
Kaisar <kaisarb@gmail.com>
2021-11-30 12:41:02 +0600

    Add files via upload

---
+++ HiLo2/src/com/kb/HiLo2.java
@@ -0,0 +1,11 @@
+package com.kb;
+import java.util.Scanner;
+public class HiLo2 {
+    void pln(String strPar1) {
+        System.out.println(strPar1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
